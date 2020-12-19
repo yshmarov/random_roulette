169286e222a6270ab26e9749b3686f0be57111e4
Yaro <yshmarov@gmail.com>
2020-12-19 17:29:48 +0000

    create or update existing bet

--- app/controllers/roulettes_controller.rb
+++ app/controllers/roulettes_controller.rb
@@ -17,31 +17,32 @@ class RoulettesController < ApplicationController
 
   def gamble
     @roulette = Roulette.find(params[:id])
-    new_size = params[:weight].to_i
+    add_weight = params[:weight].to_i
 
-    if new_size > @roulette.shares_available
+    if add_weight > @roulette.shares_available
       redirect_to root_path, alert: "There aren't so many shares left! Maximum #{@roulette.shares_available}"
-    elsif new_size > current_user.balance
+    elsif add_weight > current_user.balance
       redirect_to root_path, alert: "You don't have enough funds to make such this bet!"
     else
-      # @bet = Bet.create(roulette: @roulette, user: current_user, weight: new_size)
-      @bet = Bet.new(weight: new_size, roulette: @roulette, user: current_user)
-      if @bet.save
-        @bet.roulette.update_available_shares
-        @bet.user.update_balance
-
-        if @bet.roulette.shares_available.zero?
-          # randomizer will be here
-          weighted_random_winner = User.first
-          @roulette.update(user: weighted_random_winner)
-          weighted_random_winner.update_balance
-          Roulette.create(shares_total: @roulette.shares_total)
-          redirect_to @roulette, notice: "There's a winner!"
-        else
-          redirect_to root_path, notice: "You've made a bet!"
-        end
+      @current_bet = current_user.bets.where(roulette: @roulette).first
+      if @current_bet.present?
+        @bet = @current_bet
+        @bet.increment!(:weight, by = add_weight)
       else
-        redirect_to root_path, alert: "Something went wrong"
+        @bet = Bet.create(weight: add_weight, roulette: @roulette, user: current_user)
+      end
+      @bet.roulette.update_available_shares
+      @bet.user.update_balance
+
+      if @bet.roulette.shares_available.zero?
+        # randomizer will be here
+        weighted_random_winner = User.first
+        @roulette.update(user: weighted_random_winner)
+        weighted_random_winner.update_balance
+        Roulette.create(shares_total: @roulette.shares_total)
+        redirect_to @roulette, notice: "There's a winner!"
+      else
+        redirect_to root_path, notice: "You've made a bet!"
       end
     end
   end
