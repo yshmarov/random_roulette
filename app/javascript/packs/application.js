be8adeeece10d13dd95e9a563c8b645dd4689c88
Yaro <yshmarov@gmail.com>
2020-12-20 12:43:20 +0000

    add fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,4 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
+import "@fortawesome/fontawesome-free/css/all"
